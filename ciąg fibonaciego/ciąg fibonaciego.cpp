10c12ecedcd598b461d013c6d3ee6536c1ca97bd
Oliwer Mielcarek <100084923+rejzorx@users.noreply.github.com>
2024-05-14 13:02:51 +0200

    Add files via upload

---
+++ ciąg fibonaciego/ciąg fibonaciego.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Podaj liczbe n: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Podaj liczbe wieksza od 0" << endl;
+        return 1;
+    }
+
+    int a = 0, b = 1;
+    cout << "Ciag Fibonacciego: ";
+
+    for (int i = 0; i < n; ++i) {
+        if (i == 0) {
+            cout << a << " ";
+        }
+        else if (i == 1) {
+            cout << b << " ";
+        }
+        else {
+            int next = a + b;
+            a = b;
+            b = next;
+            cout << next << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
